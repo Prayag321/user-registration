842535f2afcb6e7cfacfbd655dc967eca6ece63a
PRAYAG <nanabhoir321@gmail.com>
2024-08-20 13:39:39 +0530

    [PRAYAG] Add: Use case 3- Check the email as per given conditions

--- test_user_registration.py
+++ test_user_registration.py
@@ -1,5 +1,5 @@
 import unittest
-from user_registration import validate_name
+from user_registration import validate_name, is_email_valid
 
 class TestUserRegistration(unittest.TestCase):
     def test_validate_name(self):
@@ -12,6 +12,17 @@ class TestUserRegistration(unittest.TestCase):
         self.assertFalse(validate_name('prayag'))
         self.assertFalse(validate_name('Deven123'))
         self.assertFalse(validate_name('Ay'))
+
+        # Valid emails
+        self.assertTrue(is_email_valid('prayag@gmail.com'))
+        self.assertTrue(is_email_valid('abc.xyz@bl.co'))
+        self.assertTrue(is_email_valid('abc@bl.co.in'))
+
+        # Invalid emails
+        self.assertFalse(is_email_valid('abc@bl'))
+        self.assertFalse(is_email_valid('abc@bl.c'))
+        self.assertFalse(is_email_valid('abc@bl.corp'))
+
 
 if __name__=="__main__":
     unittest.main()
--- user_registration.py
+++ user_registration.py
@@ -16,25 +16,55 @@ def validate_name(name):
       name(str): User input name
 
     Returns:
-      bool:True if match False otherwise
+      bool:True if match, False otherwise
     """
     pattern = "^[A-Z][A-Za-z]{2,}$"
     return bool(re.match(pattern,name))
 
-def main():
-    first_name = input("Enter the first name: ")
-    last_name = input("Enter the last name: ")
+def is_email_valid(email):
+    """
+    Description:
+      This function validates the email
 
-    if validate_name(first_name):
-        print("First name is valid")
-    else:
-        print("First name is invalid")
+    Parameters:
+      email(str): User input email
+
+    Returns:
+      bool:True if match, False otherwise
+    """
+    pattern = "^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}(\.[a-zA-Z]{2})?$"
+    return bool(re.match(pattern, email))
+
+
+def main():
+    while True:
+        first_name = input("Enter the first name (or 0 to exit): ")
+        if first_name == "0":
+            break
+        if validate_name(first_name):
+            print("First name is valid")
+        else:
+            print("First name is invalid, try again.\n")
+            continue
         
-    if validate_name(last_name):
-        print("Last name is valid")
-    else:
-        print("Last name is invalid")
+        last_name = input("Enter the last name (or 0 to exit): ")
+        if last_name == "0":
+            break
+        if validate_name(last_name):
+            print("Last name is valid")
+        else:
+            print("Last name is invalid, try again.\n")
+            continue
         
+        email = input("Enter the email (or 0 to exit): ")
+        if email == "0":
+            break
+        if is_email_valid(email):
+            print("Email is valid")
+            break
+        else:
+            print("Email is invalid, try again.\n")
+
 
 if __name__=="__main__":
     main()
